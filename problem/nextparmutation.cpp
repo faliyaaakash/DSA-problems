b07db8477503e9f98a48aaa9372cf733244b79fd
faliyaaakash <aakashfaliya1@gmail.com>
2025-07-11 23:33:28 +0530

    Initial commit

---
+++ problem/nextparmutation.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include<vector>
+using namespace std;
+vector<int> nextparmute(vector<int> &v){
+    int i=v.size()-1;
+    while(i>0){
+        if(v[i+1]<v[i]){
+            
+        }
+    }
+}
+int main(){
+return 0;
+}

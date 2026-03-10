97ddb21c32b277367f459c8a11ee43053d426a71
faliyaaakash <aakashfaliya1@gmail.com>
2026-03-10 18:21:53 +0530

    Time: 4 ms (22.11%) | Memory: 10.3 MB (62.51%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,37 +2,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
-        int i = 0;
-        string word = "";
+       string result="";
+       int i=s.length()-1;
 
-        while (i < s.length()) {
-            if (s[i] != ' ') {
-                word += s[i];
-            } else {
-                if (!word.empty()) {
-                    st.push(word);
-                  
-                }
-                  word = "";
-            }
-            i++;
-        }
-
-        // Push last word if it's not empty
-        if (!word.empty()) {
-            st.push(word);
-        }
-
-        string result = "";
-        while (!st.empty()) {
-            result += st.top();
-            st.pop();
-            if (!st.empty()) {
-                result += " ";
-            }
-        }
-
-        return result;
+       while(i>=0){
+         while(i>=0 && s[i]==' '){
+            i--;
+         }
+          if (i < 0) break;
+         int end=i;
+         while(i>=0 && s[i]!=' '){
+            i--;
+         }
+         string word=s.substr(i+1,end-i);
+         if(!result.empty()){
+            result+=' ';
+         }
+         result+=word;
+       }
+       return result;
     }
 };

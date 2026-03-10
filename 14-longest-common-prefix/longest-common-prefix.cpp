fb47de19a24a76ae2b53924da8bee65091e1aaa6
faliyaaakash <aakashfaliya1@gmail.com>
2026-03-11 00:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (16.28%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,16 +3,16 @@ class Solution {
      
     string longestCommonPrefix(vector<string>& strs) {
         string prefix="";
-
-        for(int i=0;i<strs[0].length();i++){
-             char ch=strs[0][i];
-             for(int j=1;j<strs.size();j++){
-                if(strs[j][i]!=ch){
-                    return prefix;
-                }
-             }
-            prefix+=ch;
-        } 
+        int n=strs.size()-1;
+        sort(strs.begin(),strs.end());
+        int len=min(strs[0].length(),strs[n].length());
+        string s1=strs[0],s2=strs[n];
+        for(int i=0;i<len;i++){
+            if(s1[i]!=s2[i]){
+                break;
+            }
+            prefix+=s1[i];
+        }
         return prefix;
     }
 };

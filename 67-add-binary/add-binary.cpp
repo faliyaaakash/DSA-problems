69a269aa62ababa5cb8ada6a4c7c71b12b5d9b23
faliyaaakash <aakashfaliya1@gmail.com>
2026-03-11 22:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (96.78%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int c=0;
+        string result;
+        int p1=a.length()-1,p2=b.length()-1;
+        int total=0;
+        while(p1>=0 || p2>=0 || c){
+           total=c;
+            if(p1>=0){
+                   total+=(a[p1]-'0');
+                   p1--;
+            }
+            if(p2>=0){
+                   total+=(b[p2]-'0');
+                   p2--;
+            }
+            result+=(total%2+'0');
+            c=total/2;
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};

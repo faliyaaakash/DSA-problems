324adf3af2fc577b0ee6cf93442245d25fb7f0b4
faliyaaakash <aakashfaliya1@gmail.com>
2026-06-01 10:04:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.98%) - LeetSync

--- 2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.cpp
+++ 2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.cpp
@@ -2,22 +2,6 @@ class Solution {
 public:
     int distinctIntegers(int n) {
         if(n==1)return 1;
-        int arr[100]={0};
-        int i=n;
-        while(i>=0){
-            for(int j=0;j<i;j++){
-                if((i+1)%(j+1)==1){
-                    arr[j]++;
-                }
-            }
-            i--;
-        }
-        int count=0;
-        for(int k=0;k<n;k++){
-           if(arr[k]>=0){
-             count++;
-           }
-        }
-        return count-1;
+        return n-1;
     }
 };

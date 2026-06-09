9fbfd824016bb0c9e7ff123a7a7281e67736ee07
faliyaaakash <aakashfaliya1@gmail.com>
2026-06-09 23:28:49 +0530

    Time: 1 ms (34.08%) | Memory: 20.8 MB (75.75%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-         map<int,int> col,row;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        bool isFirstColZero = false;
 
-            for(int i=0;i<matrix.size();i++){
-               for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0){
-                    col[j]++;
-                    row[i]++;
-                }
-               }
+        // Step 1: Use first row and first column as markers
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                isFirstColZero = true; // Flag for the first column
             }
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
 
-            for(int i=0;i<matrix.size();i++){
-               for(int j=0;j<matrix[i].size();j++){
-                if(col[j] || row[i]){
-                    matrix[i][j]=0;
+        // Step 2: Update the inner matrix from bottom-up (or top-down)
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
-               }
             }
+        }
+
+        // Step 3: Handle the first column
+        if (isFirstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
